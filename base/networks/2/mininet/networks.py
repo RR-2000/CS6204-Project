ccc5adad90a19ec224113a04c85c9e50ebbc937f
Ramanathan <e1660997@u.nus.edu>
2026-03-26 15:08:32 +0800

    Implement 2 way testing

---
+++ base/networks/2/mininet/networks.py
@@ -0,0 +1,237 @@
+import os
+import sys
+
+from mininet.node import OVSSwitch
+from mininet.topo import Topo
+
+SCRIPT_DIRECTORY = os.path.abspath(
+    os.path.dirname(__file__)
+)
+REPOSITORY_DIRECTORY = os.path.join(
+    SCRIPT_DIRECTORY,
+    "../../../"
+)
+TASKS_DIRECTORY = os.path.join(
+    REPOSITORY_DIRECTORY,
+    "tasks"
+)
+TASKS_SOURCE_DIRECTORY = os.path.join(
+    TASKS_DIRECTORY,
+    "2"
+)
+FRR_CONFIGURATION_DIRECTORY = os.path.join(
+    SCRIPT_DIRECTORY,
+    "../frr/"
+)
+
+sys.path.append(REPOSITORY_DIRECTORY)
+
+# pylint: disable=E0401,C0413
+from common.p4.functions import HelperFunctions
+from common.mininet.nodes import Client, P4Switch, FRRRouter
+
+class Topology(Topo):
+    def build(
+        self,
+        *args,
+        **params
+    ):
+        hosts_mac_addresses = {}
+
+        hosts_mac_addresses["as1r1"] = {
+            "as1r1-eth0": "f0:00:0a:01:01:00",
+            "as1r1-eth1": "f0:00:0a:01:01:01",
+        }
+        hosts_mac_addresses["as2r1"] = {
+            "as2r1-eth0": "f0:00:0a:01:02:00",
+            "as2r1-eth1": "f0:00:0a:01:02:01",
+            "as2r1-eth2": "f0:00:0a:01:02:02",
+        }
+        hosts_mac_addresses["as3r1"] = {
+            "as3r1-eth0": "f0:00:0a:01:03:00",
+            "as3r1-eth1": "f0:00:0a:01:03:01",
+            "as3r1-eth2": "f0:00:0a:01:03:02",
+        }
+        hosts_mac_addresses["as4r1"] = {
+            "as4r1-eth0": "f0:00:0a:01:04:00",
+            "as4r1-eth1": "f0:00:0a:01:04:01",
+            "as4r1-eth2": "f0:00:0a:01:04:02",
+        }
+
+        hosts_mac_addresses["as1h1"] = {"as1h1-eth0": "f0:00:0a:00:01:00"}
+        hosts_mac_addresses["as1h2"] = {"as1h2-eth0": "f0:00:0a:00:01:01"}
+        hosts_mac_addresses["as2h1"] = {"as2h1-eth0": "f0:00:0a:00:02:00"}
+        hosts_mac_addresses["as2h2"] = {"as2h2-eth0": "f0:00:0a:00:02:01"}
+        hosts_mac_addresses["as3h1"] = {"as3h1-eth0": "f0:00:0a:00:03:00"}
+        hosts_mac_addresses["as3h2"] = {"as3h2-eth0": "f0:00:0a:00:03:01"}
+        hosts_mac_addresses["as4h1"] = {"as4h1-eth0": "f0:00:0a:00:04:00"}
+        hosts_mac_addresses["as4h2"] = {"as4h2-eth0": "f0:00:0a:00:04:01"}
+
+        # SDX P4 switch
+        ixp1s1 = self.addSwitch(
+            "ixp1s1",
+            cls=P4Switch,
+            identifier=1,
+            thrift_port=9091,
+            grpc_address="0.0.0.0",
+            grpc_port=50001,
+        )
+
+        as1r1 = self.addNode(
+            "as1r1",
+            cls=FRRRouter,
+            zebraConfigFile=os.path.join(FRR_CONFIGURATION_DIRECTORY, "as1r1-zebra.conf"),
+            bgpConfigFile=os.path.join(FRR_CONFIGURATION_DIRECTORY, "as1r1-bgp.conf"),
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as1r1"]) +
+                [
+                    HelperFunctions.generate_add_loopback_interface_ip_command("10.100.1.1/32"),
+                    HelperFunctions.generate_set_static_arp_command("10.0.0.2", hosts_mac_addresses["as2r1"]["as2r1-eth1"]),
+                    HelperFunctions.generate_set_static_arp_command("10.0.0.3", hosts_mac_addresses["as3r1"]["as3r1-eth1"]),
+                ]
+            ),
+        )
+        as2r1 = self.addNode(
+            "as2r1",
+            cls=FRRRouter,
+            zebraConfigFile=os.path.join(FRR_CONFIGURATION_DIRECTORY, "as2r1-zebra.conf"),
+            bgpConfigFile=os.path.join(FRR_CONFIGURATION_DIRECTORY, "as2r1-bgp.conf"),
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as2r1"]) +
+                [
+                    HelperFunctions.generate_add_loopback_interface_ip_command("10.100.2.1/32"),
+                    HelperFunctions.generate_set_static_arp_command("10.0.0.1", hosts_mac_addresses["as1r1"]["as1r1-eth1"]),
+                ]
+            ),
+        )
+        as3r1 = self.addNode(
+            "as3r1",
+            cls=FRRRouter,
+            zebraConfigFile=os.path.join(FRR_CONFIGURATION_DIRECTORY, "as3r1-zebra.conf"),
+            bgpConfigFile=os.path.join(FRR_CONFIGURATION_DIRECTORY, "as3r1-bgp.conf"),
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as3r1"]) +
+                [
+                    HelperFunctions.generate_add_loopback_interface_ip_command("10.100.3.1/32"),
+                    HelperFunctions.generate_set_static_arp_command("10.0.0.1", hosts_mac_addresses["as1r1"]["as1r1-eth1"]),
+                ]
+            ),
+        )
+        as4r1 = self.addNode(
+            "as4r1",
+            cls=FRRRouter,
+            zebraConfigFile=os.path.join(FRR_CONFIGURATION_DIRECTORY, "as4r1-zebra.conf"),
+            bgpConfigFile=os.path.join(FRR_CONFIGURATION_DIRECTORY, "as4r1-bgp.conf"),
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as4r1"]) +
+                [HelperFunctions.generate_add_loopback_interface_ip_command("10.100.4.1/32")]
+            ),
+        )
+
+        as1s1 = self.addSwitch("as1s1", cls=OVSSwitch)
+        as2s1 = self.addSwitch("as2s1", cls=OVSSwitch)
+        as3s1 = self.addSwitch("as3s1", cls=OVSSwitch)
+        as4s1 = self.addSwitch("as4s1", cls=OVSSwitch)
+
+        as1h1 = self.addHost(
+            "as1h1", cls=Client,
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as1h1"]) +
+                [HelperFunctions.generate_set_default_route_command("10.1.0.1")]
+            ),
+        )
+        as1h2 = self.addHost(
+            "as1h2", cls=Client,
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as1h2"]) +
+                [HelperFunctions.generate_set_default_route_command("10.1.0.1")]
+            ),
+        )
+        as2h1 = self.addHost(
+            "as2h1", cls=Client,
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as2h1"]) +
+                [HelperFunctions.generate_set_default_route_command("10.2.0.1")]
+            ),
+        )
+        as2h2 = self.addHost(
+            "as2h2", cls=Client,
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as2h2"]) +
+                [HelperFunctions.generate_set_default_route_command("10.2.0.1")]
+            ),
+        )
+        as3h1 = self.addHost(
+            "as3h1", cls=Client,
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as3h1"]) +
+                [HelperFunctions.generate_set_default_route_command("10.3.0.1")]
+            ),
+        )
+        as3h2 = self.addHost(
+            "as3h2", cls=Client,
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as3h2"]) +
+                [HelperFunctions.generate_set_default_route_command("10.3.0.1")]
+            ),
+        )
+        as4h1 = self.addHost(
+            "as4h1", cls=Client,
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as4h1"]) +
+                [HelperFunctions.generate_set_default_route_command("10.4.0.1")]
+            ),
+        )
+        as4h2 = self.addHost(
+            "as4h2", cls=Client,
+            configCmds=(
+                HelperFunctions.generate_set_interface_mac_commands(hosts_mac_addresses["as4h2"]) +
+                [HelperFunctions.generate_set_default_route_command("10.4.0.1")]
+            ),
+        )
+
+        # AS1 internal
+        self.addLink(as1r1, as1s1, intfName1="as1r1-eth0", params1={"ip": "10.1.0.1/24"}, intfName2="as1s1-eth0")
+        self.addLink(as1h1, as1s1, intfName1="as1h1-eth0", params1={"ip": "10.1.0.101/24"}, intfName2="as1s1-eth1")
+        self.addLink(as1h2, as1s1, intfName1="as1h2-eth0", params1={"ip": "10.1.0.102/24"}, intfName2="as1s1-eth2")
+
+        # AS2 internal
+        self.addLink(as2r1, as2s1, intfName1="as2r1-eth0", params1={"ip": "10.2.0.1/24"}, intfName2="as2s1-eth0")
+        self.addLink(as2h1, as2s1, intfName1="as2h1-eth0", params1={"ip": "10.2.0.101/24"}, intfName2="as2s1-eth1")
+        self.addLink(as2h2, as2s1, intfName1="as2h2-eth0", params1={"ip": "10.2.0.102/24"}, intfName2="as2s1-eth2")
+
+        # AS3 internal
+        self.addLink(as3r1, as3s1, intfName1="as3r1-eth0", params1={"ip": "10.3.0.1/24"}, intfName2="as3s1-eth0")
+        self.addLink(as3h1, as3s1, intfName1="as3h1-eth0", params1={"ip": "10.3.0.101/24"}, intfName2="as3s1-eth1")
+        self.addLink(as3h2, as3s1, intfName1="as3h2-eth0", params1={"ip": "10.3.0.102/24"}, intfName2="as3s1-eth2")
+
+        # AS4 internal
+        self.addLink(as4r1, as4s1, intfName1="as4r1-eth0", params1={"ip": "10.4.0.1/24"}, intfName2="as4s1-eth0")
+        self.addLink(as4h1, as4s1, intfName1="as4h1-eth0", params1={"ip": "10.4.0.101/24"}, intfName2="as4s1-eth1")
+        self.addLink(as4h2, as4s1, intfName1="as4h2-eth0", params1={"ip": "10.4.0.102/24"}, intfName2="as4s1-eth2")
+
+        # AS1, AS2, AS3 to SDX
+        self.addLink(as1r1, ixp1s1, intfName1="as1r1-eth1", params1={"ip": "10.0.0.1/24"}, intfName2="ixp1s1-eth1")
+        self.addLink(as2r1, ixp1s1, intfName1="as2r1-eth1", params1={"ip": "10.0.0.2/24"}, intfName2="ixp1s1-eth2")
+        self.addLink(as3r1, ixp1s1, intfName1="as3r1-eth1", params1={"ip": "10.0.0.3/24"}, intfName2="ixp1s1-eth3")
+
+        # AS4 to AS2 direct link - fast path
+        self.addLink(
+            as4r1, as2r1,
+            intfName1="as4r1-eth1", params1={"ip": "10.0.1.2/24"},
+            intfName2="as2r1-eth2", params2={"ip": "10.0.1.1/24"},
+            # bw=0.001,
+        )
+
+        # AS4 to AS3 direct link - backup path
+        self.addLink(
+            as4r1, as3r1,
+            intfName1="as4r1-eth2", params1={"ip": "10.0.2.2/24"},
+            intfName2="as3r1-eth2", params2={"ip": "10.0.2.1/24"},
+            # bw=1,
+        )
+
+# pylint: disable=W0108
+topos = {
+    "topology": (lambda: Topology())
+}
